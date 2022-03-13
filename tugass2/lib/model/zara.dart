d9cdfec7f59b21df335425c8848a2fa2b07d6d9b
Aswira Yuniar <aswiraaa27@gmail.com>
2022-03-14 01:14:58 +0700

    first commit

---
+++ tugass2/lib/model/zara.dart
@@ -0,0 +1,54 @@
+class Zara {
+  String image;
+  String name;
+  int price;
+
+  Zara({
+    required this.image,
+    required this.name,
+    required this.price,
+  });
+}
+
+var getData = [
+  Zara(
+    image:
+    "https://static.zara.net/photos///2022/V/0/1/p/8727/014/200/2/w/452/8727014200_1_1_1.jpg?ts=1646826352758",
+    name: "JEANS Z1975 HIGH RISE FLARE",
+    price: 749000,
+  ),
+  Zara(
+    image: "https://static.zara.net/photos///2022/V/0/1/p/5252/013/400/2/w/452/5252013400_1_1_1.jpg?ts=1646822728489",
+    name: "MID-RISE WIDE STRAIGHT JEANS",
+    price: 749000,
+  ),
+  Zara(
+    image: "https://static.zara.net/photos///2022/V/0/1/p/7215/211/406/2/w/608/7215211406_1_1_1.jpg?ts=1646759105479",
+    name: "Z1975 BAGGY PAPERBAG JEANS",
+    price: 749000,
+  ),
+  Zara(
+    image: "https://static.zara.net/photos///2022/V/0/1/p/6147/058/520/2/w/452/6147058520_2_4_1.jpg?ts=1646915399651",
+    name: "BOOTCUT JEANS",
+    price: 749000,
+  ),
+  Zara(
+    image: "https://static.zara.net/photos///2022/V/0/1/p/6147/058/633/2/w/452/6147058633_1_1_1.jpg?ts=1646915427707",
+    name: "MID-RISE WIDE STRAIGHT JEANS",
+    price: 749000,
+  ),Zara(
+    image: "https://static.zara.net/photos///2022/V/0/1/p/5862/078/406/2/w/452/5862078406_1_1_1.jpg?ts=1646733340946",
+    name: "Z1975 MOM FIT RHINESTONE JEANS",
+    price: 749000,
+  ),
+  Zara(
+    image: "https://static.zara.net/photos///2022/V/0/1/p/6147/183/406/2/w/452/6147183406_1_1_1.jpg?ts=1646733368946",
+    name: "Z1975 SUPER HIGH-RISE STRAIGHT JEANS",
+    price: 749000,
+  ),
+  Zara(
+    image: "https://static.zara.net/photos///2022/V/0/1/p/1889/061/406/2/w/449/1889061406_1_1_1.jpg?ts=1646212825397",
+    name: "SLIM FIT JEANS",
+    price: 749000,
+  ),
+];
